a5f2f57a5902d1cb6e6bc568f05f3f10f2a419a0
michalhagos <mhayele3@gmail.com>
2026-06-22 22:55:22 -0500

    Added needed annotation to ShoppingCartController

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,5 +1,9 @@
 package org.yearup.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.User;
 import org.yearup.service.ShoppingCartService;
@@ -8,7 +12,11 @@ import org.yearup.service.UserService;
 import java.security.Principal;
 
 // convert this class to a REST controller
-// only logged in users should have access to these actions
+// only logged-in users should have access to these actions
+@RestController
+@RequestMapping("cart")
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
 public class ShoppingCartController
 {
     // a shopping cart controller depends on the service layer
@@ -20,7 +28,7 @@ public class ShoppingCartController
     // each method in this controller requires a Principal object as a parameter
     public ShoppingCart getCart(Principal principal)
     {
-        // get the currently logged in username
+        // get the currently logged-in username
         String userName = principal.getName();
         // find database user by username
         User user = userService.getByUserName(userName);
