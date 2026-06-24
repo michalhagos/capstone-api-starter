9f4e5dbe43f903a4eb56b9a0b03dc5f5326b8c37
michalhagos <mhayele3@gmail.com>
2026-06-24 09:40:18 -0500

    Added fields and constructor to the OrdersController class

--- src/main/java/org/yearup/controllers/OrdersController.java
+++ src/main/java/org/yearup/controllers/OrdersController.java
@@ -3,11 +3,19 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yearup.service.OrderService;
+import org.yearup.service.UserService;
 
 @RestController
 @RequestMapping("orders")
 @CrossOrigin
 @PreAuthorize("isAuthenticated()")
 public class OrdersController {
+    private final OrderService orderService;
+    private final UserService userService;
 
+    public OrdersController(OrderService orderService, UserService userService) {
+        this.orderService = orderService;
+        this.userService = userService;
+    }
 }
