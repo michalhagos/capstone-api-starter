2dc497619a626f13d10e3c383ef5a38f009bd38e
michalhagos <mhayele3@gmail.com>
2026-06-24 11:33:49 -0500

    Added fields and annotations to OrderLineItem class

--- src/main/java/org/yearup/models/OrderLineItem.java
+++ src/main/java/org/yearup/models/OrderLineItem.java
@@ -1,10 +1,27 @@
 package org.yearup.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "order_line_items")
 public class OrderLineItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_line_item_id")
+    private int orderLineItemId;
 
+    @Column(name = "order_id")
+    private int orderId;
+
+    @Column(name = "product_id")
+    private int productId;
+
+    @Column(name = "sales_price")
+    private double salesPrice;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "discount")
+    private double discount;
 }
