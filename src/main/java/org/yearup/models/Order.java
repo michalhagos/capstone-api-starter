1c0c3ba9bee16636f49de6d806f544b6d3d826f1
michalhagos <mhayele3@gmail.com>
2026-06-24 11:22:48 -0500

    Added getters and setters to Order class

--- src/main/java/org/yearup/models/Order.java
+++ src/main/java/org/yearup/models/Order.java
@@ -25,4 +25,68 @@ public class Order {
     private String zip;
     @Column(name = "shipping_amount")
     private double shippingAmount;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public double getShippingAmount() {
+        return shippingAmount;
+    }
+
+    public void setShippingAmount(double shippingAmount) {
+        this.shippingAmount = shippingAmount;
+    }
 }
