6bc2407e5646feb640007393a2e03c50e0ada327
michalhagos <mhayele3@gmail.com>
2026-06-24 11:36:17 -0500

    Added getters and setters to OrderLineItem class

--- src/main/java/org/yearup/models/OrderLineItem.java
+++ src/main/java/org/yearup/models/OrderLineItem.java
@@ -24,4 +24,53 @@ public class OrderLineItem {
 
     @Column(name = "discount")
     private double discount;
+
+    public int getOrderLineItemId() {
+        return orderLineItemId;
+    }
+
+    public void setOrderLineItemId(int orderLineItemId) {
+        this.orderLineItemId = orderLineItemId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public double getSalesPrice() {
+        return salesPrice;
+    }
+
+    public void setSalesPrice(double salesPrice) {
+        this.salesPrice = salesPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
 }
