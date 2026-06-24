e01e9064774d7a5d14d0c29bcabe8f40f4026d39
michalhagos <mhayele3@gmail.com>
2026-06-24 11:15:58 -0500

    Added fields to Order class

--- src/main/java/org/yearup/models/Order.java
+++ src/main/java/org/yearup/models/Order.java
@@ -1,9 +1,28 @@
 package org.yearup.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private int orderId;
+    @Column(name = "user_id")
+    private int userId;
+    @Column(name = "date")
+    private LocalDateTime date;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "state")
+    private String state;
+    @Column(name = "zip")
+    private String zip;
+    @Column(name = "shipping_amount")
+    private double shippingAmount;
 }
