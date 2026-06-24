c95eb0a4b56a164cc17ce18182e6d7bb3b1583da
michalhagos <mhayele3@gmail.com>
2026-06-24 12:25:44 -0500

    added fields and constructor to order service class

--- src/main/java/org/yearup/service/OrderService.java
+++ src/main/java/org/yearup/service/OrderService.java
@@ -1,7 +1,23 @@
 package org.yearup.service;
 import org.springframework.stereotype.Service;
+import org.yearup.repository.OrderLineItemRepository;
+import org.yearup.repository.OrderRepository;
 
 @Service
 public class OrderService {
+    private final OrderRepository orderRepository;
+    private final OrderLineItemRepository orderLineItemRepository;
+    private final ProfileService profileService;
+    private final ShoppingCartService shoppingCartService;
 
+    public OrderService(OrderRepository orderRepository,
+                        OrderLineItemRepository orderLineItemRepository,
+                        ProfileService profileService,
+                        ShoppingCartService shoppingCartService)
+    {
+        this.orderRepository = orderRepository;
+        this.orderLineItemRepository = orderLineItemRepository;
+        this.profileService = profileService;
+        this.shoppingCartService = shoppingCartService;
+    }
 }
